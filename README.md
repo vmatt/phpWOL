e28b1bcf74036ab4ff1ced80e603e010ab505121
Mate Valko <you@example.com>
2024-08-28 11:49:53 +0200

    new version with restart & shutdown via lightweight winRM

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # phpWOL
 Forked from [castab/phpWOL](https://github.com/castab/phpWOL). Replaced the backend with PHP_WOL developed by [Radovan Janjic](https://radovanjanjic.com)
+
 ## Modifications
 - Replace backend to be able to send Magic Packet to xxx.xxx.xxx.255 broadcast address.
+- Added shutdown and restart functionality using a lightweight WinRmClient (based on [vmatt/phpwinrm](https://github.com/vmatt/phpwinrm)).
+
+## Features
+- Wake-on-LAN: Turn on computers remotely that have WoL enabled.
+- Shutdown: Remotely shut down Windows computers.
+- Restart: Remotely restart Windows computers.
+
 ## Requirements
 - PHP
-  - Sockets module
+  - Sockets (for Wake-on-LAN)
+  - CURL (for WinRM operations)
 - WebServer
-## A web-driven Wake-on-LAN Service
-php driven Wake on LAN webapp for remote computer startup
+- Windows computers with WinRM enabled (for shutdown and restart operations)
 
-The purpose of this webapp is to remotely turn on computers that have WoL enabled.
+## A web-driven Wake-on-LAN and Remote Management Service
+This webapp allows for remote computer startup, shutdown, and restart operations.
 
-Many routers disable/ignore the broadcast address of a subnet that originate from outside of the local network.  To circumvent this, a PHP server placed within the network is able to receive a command in the form of a submission and then dynamically generate a WoL packet to specific computers in the same subnet.
+The Wake-on-LAN functionality works by having a PHP server within the network receive a command and then dynamically generate a WoL packet to specific computers in the same subnet. This circumvents the issue of many routers disabling/ignoring broadcast addresses from outside the local network.
 
-Much of the credit goes to, Toni Uebernickel <tuebernickel@whitestarprogramming.de>, who coded the entire WoL packet generator function.  I only wrapped it into a web page and added a few more tweaks:
+The shutdown and restart functionality uses Windows Remote Management (WinRM) to securely execute commands on remote Windows machines.
 
-1. Some authentication (not completely secure, but better than nothing)
-2. A GUI in the form of a webpage
+## Credits
+- Toni Uebernickel <tuebernickel@whitestarprogramming.de>: Original WoL packet generator function
+- [Radovan Janjic](https://radovanjanjic.com): PHP_WOL backend
+- [vmatt/phpwinrm](https://github.com/vmatt/phpwinrm): Lightweight WinRM client
+
+## Security Note
+While basic authentication is implemented, it's recommended to further secure this application, especially when exposed to the internet. Consider implementing stronger authentication methods and ensuring all communications are encrypted.
--- wol_action.php
+++ wol_action.php
@@ -1,28 +1,165 @@
 <?php
-include_once ('wol_functions.php');
+require_once __DIR__ . '/winrm.php';
+
+
+/******************************************************************
+ * 
+ * Projectname:   PHP_WOL - PHP Wake On Lan
+ * Version:       1.0
+ * Author:        Radovan Janjic <hi@radovanjanjic.com>
+ * Last modified: 29 09 2014
+ * Copyright (C): 2014 IT-radionica.com, All Rights Reserved
+ * 
+ * GNU General Public License (Version 2, June 1991)
+ *
+ * This program is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free
+ * Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ * 
+ ******************************************************************/
+/** Example:
+ * PHP_WOL::send('192.168.1.2', '01:23:45:67:89:ab', 9);
+ */
+
+class PHP_WOL {
+	
+	private static $socket = 0;
+	
+	/** Send WOL package
+	 * @param	string		$addr		- IP address
+	 * @param	string		$mac		- Media access control address (MAC)
+	 * @param	integer		$port		- Port number at which the data will be sent
+	 * @return	boolean
+	 */
+	public static function send($addr, $mac, $port = 9) {
+		// Throw exception if extension is not loaded
+		if (!extension_loaded('sockets')) {
+			self::throwError("Error: The sockets extension is not loaded!");
+		}
+
+		// Check if $addr is valid IP, if not try to resolve host
+		if (!filter_var($addr, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+			// Try to get the IPv4 address of a given host name
+			$originalAddr = gethostbyname($addr);
+			if ($originalAddr == $addr) {
+				self::throwError('Error: Domain name is unresolvable or IP address is invalid!');
+			} else {
+				$addr = $originalAddr;
+			}
+		}
+		$addr = explode('.', $addr);
+		$addr[3] = '255';
+		$addr = implode('.', $addr);
+		// self::throwError($addr);
+		$macHex = str_replace(array(':', '-'), '', $mac);
+		
+		// Throw exception if mac address is not valid
+		if (!ctype_xdigit($macHex) || strlen($macHex) != 12) {
+			self::throwError('Error: Mac address is invalid!');
+		}
+		
+		// Magic packet
+		$packet = str_repeat(chr(255), 6) . str_repeat(pack('H12', $macHex), 16);
+		
+		// Send to the broadcast address using UDP
+		self::$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+		
+		if (self::$socket !== false) {
+		
+			// Set socket option
+			if (!socket_set_option(self::$socket, SOL_SOCKET, SO_BROADCAST, TRUE)) {
+				self::throwError();
+			}
+			
+			// Send magic packet
+			if (socket_sendto(self::$socket, $packet, strlen($packet), 0, $addr, $port) !== FALSE) {
+				socket_close(self::$socket);
+				return TRUE;
+			}
+		}
+		self::throwError();
+	}
+	
+	private static function throwError($msg = NULL) {
+		if (empty($msg)) {
+			$msg = "Error (" . socket_last_error(self::$socket) . "): " . socket_strerror(socket_last_error(self::$socket));
+		}
+		throw new Exception($msg);
+	}
+}
+
+function wakeUp($mac, $broadcastIP)
+{
+    PHP_WOL::send($broadcastIP, $mac);
+    return "Wake-up command sent successfully.";
+}
+
+function restartComputer($ipAddress, $hostName)
+{
+    return executeShutdownCommand($ipAddress, $hostName, '/r');
+}
+
+function shutdownComputer($ipAddress, $hostName)
+{
+    return executeShutdownCommand($ipAddress, $hostName, '/s');
+}
+
+function executeShutdownCommand($ipAddress, $hostName, $shutdownFlag)
+{
+    $command = "shutdown {$shutdownFlag} /t 0";
+    // $command = "ipconfig";
+    $client = new WinRmClient($ipAddress, $hostName, WINRM_PASSWORD);
+    $result = $client->execute_command($command);
+    
+    if (strpos($result, 'ERROR') !== false) {
+        throw new Exception("Failed to send " . ($shutdownFlag === '/r' ? 'restart' : 'shutdown') . " command. Error: " . $result);
+    }
+    
+    return ($shutdownFlag === '/r' ? 'Restart' : 'Shutdown') . " command sent successfully. Result: " . $result;
+}
 
 $data = json_decode(file_get_contents('php://input'), true);
 header('Content-Type: application/json');
 
-$WOLresponse = new stdClass();
+$response = new stdClass();
 
-if (isset($data['hostName']) && isset($data['ipAddress']) && isset($data['macAddress'])) {
-    $msg = 'Success';
-    wakeUp($data['macAddress'], $data['ipAddress'], $msg);
-    if ($msg != 'Success') {
-        http_response_code(500);
-        $WOLresponse->status = false;
-        $WOLresponse->error = $msg;
-    } else {
-        http_response_code(200);
-        $WOLresponse->status = "OK";
+try {
+    if (!isset($data['host']) || !isset($data['action'])) {
+        throw new Exception('Invalid request.');
     }
-    echo json_encode($WOLresponse);
-} else {
-    $WOLresponse->status = false;
-    $WOLresponse->error = 'Invalid request.';
-    http_response_code(400);
-    echo json_encode($WOLresponse);
+
+    $host = $data['host'];
+    $action = $data['action'];
+
+    switch ($action) {
+        case 'Wake':
+            $message = wakeUp($host['macAddress'], $host['ipAddress']);
+            break;
+        case 'Restart':
+            $message = restartComputer($host['ipAddress'], $host['hostName']);
+            break;
+        case 'Shutdown':
+            $message = shutdownComputer($host['ipAddress'], $host['hostName']);
+            break;
+        default:
+            throw new Exception('Invalid action.');
+    }
+
+    $response->status = "OK";
+    $response->message = $message;
+} catch (Exception $e) {
+    $response->status = "Error";
+    $response->error = $e->getMessage();
 }
 
+echo json_encode($response);
 ?>
+
